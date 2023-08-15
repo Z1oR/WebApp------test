b4e30ffcc5875b8289f00bb8c577f3af719c0686
Z1oR <79359655+Z1oR@users.noreply.github.com>
2023-08-15 16:40:23 +0300

    app.js

--- app.js
+++ app.js
@@ -46,16 +46,14 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
+		tg.MainButton.setText("Нажмите чтобы оплатить");
 		item = "1";
 		tg.MainButton.show();
 	}
@@ -66,7 +64,7 @@ btn2.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
+		tg.MainButton.setText("Нажмите чтобы оплатить");
 		item = "2";
 		tg.MainButton.show();
 	}
@@ -77,49 +75,24 @@ btn3.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
+		tg.MainButton.setText("Нажмите чтобы оплатить");
 		item = "3";
 		tg.MainButton.show();
 	}
 });
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
+let data = {
+	item: item,
+	
+}
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
+tg.sendData(JSON.stringify(data))
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+// 	tg.sendData(item);
+// });
+
 
 
 let usercard = document.getElementById("usercard");
