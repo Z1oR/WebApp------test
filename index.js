f3a628ce3ffedcc1f7e002101eab63ddfa8846f3
Z1oR <79359655+Z1oR@users.noreply.github.com>
2024-05-28 23:27:52 +0300

    index.js

--- index.js
+++ index.js
@@ -4,3 +4,43 @@ tg.expand();
 
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
+
+
+
+let BlockMain = document.querySelector(".BlockMain");
+let BlockBoost = document.querySelector(".BlockBoost");
+let BlockTask = document.querySelector(".BlockTask");
+
+
+let Main = document.querySelector("#Main");
+let taskBlock = document.querySelector(".taskBlock");
+let BoostBlock = document.querySelector(".BoostBlock");
+
+BlockMain.addEventListener("click", () => {
+    Main.style.display = "block";
+    taskBlock.style.display = "none";
+    BoostBlock.style.display = "none";
+
+    BlockMain.style.backgroundColor = "#30e0ec"
+    BlockTask.style.backgroundColor = "white"
+    BlockBoost.style.backgroundColor = "white"
+})
+BlockBoost.addEventListener("click", () => {
+    Main.style.display = "none";
+    taskBlock.style.display = "none";
+    BoostBlock.style.display = "block";
+
+
+    BlockMain.style.backgroundColor = "white"
+    BlockTask.style.backgroundColor = "white"
+    BlockBoost.style.backgroundColor = "#30e0ec"
+})
+BlockTask.addEventListener("click", () => {
+    Main.style.display = "none";
+    taskBlock.style.display = "block";
+    BoostBlock.style.display = "none";
+
+    BlockTask.style.backgroundColor = "#30e0ec"
+    BlockMain.style.backgroundColor = "white"
+    BlockBoost.style.backgroundColor = "white"
+})
